b56c8fee166709ba8551b6328b68df788d4c35b1
Aaron W <awong540@my.bcit.ca>
2023-03-28 18:47:21 -0700

    cancel and new note buttons noe hide/show the text area and misc buttons

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,20 @@
 function darktoggle() {
-    document.body.classList.toggle("darkmode");
-    if(document.querySelector('.darktheme').innerText=='Dark Theme')document.querySelector('.darktheme').innerText='Light Theme';
-    else
-      document.querySelector('.darktheme').innerText='Dark Theme';
-  }
-  
+  document.body.classList.toggle("darkmode");
+  if (document.querySelector(".darktheme").innerText == "Dark Theme")
+    document.querySelector(".darktheme").innerText = "Light Theme";
+  else document.querySelector(".darktheme").innerText = "Dark Theme";
+}
+
+function cancel() {
+  document.getElementById("notearea").style.display = "none";
+  document.getElementById("save").style.display = "none";
+  document.getElementById("cancel").style.display = "none";
+}
+
+function newnote() {
+  document.getElementById("notearea").style.display = "block";
+  document.getElementById("save").style.display = "block";
+  document.getElementById("cancel").style.display = "block";
+  document.getElementById("notearea").value = "Insert your notes here!";
+}
+
