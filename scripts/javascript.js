809f61d0e97074ad6ba04a98bd04bb82ac4498cd
Aaron W <awong540@my.bcit.ca>
2023-04-07 21:34:03 -0700

    save function works, but retrieving notes is broken

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,4 @@
+document.getElementById("darkmode").addEventListener("click", darktoggle);
 function darktoggle() {
   document.body.classList.toggle("darkmode");
   if (document.querySelector(".darktheme").innerText == "Dark Theme")
@@ -5,12 +6,14 @@ function darktoggle() {
   else document.querySelector(".darktheme").innerText = "Dark Theme";
 }
 
+document.getElementById("cancel").addEventListener("click", cancel);
 function cancel() {
   document.getElementById("notearea").style.display = "none";
   document.getElementById("save").style.display = "none";
   document.getElementById("cancel").style.display = "none";
 }
 
+document.getElementById("newnote").addEventListener("click", newnote);
 function newnote() {
   document.getElementById("notearea").style.display = "block";
   document.getElementById("save").style.display = "block";
@@ -18,3 +21,43 @@ function newnote() {
   document.getElementById("notearea").value = "Insert your notes here!";
 }
 
+let notesArray = [
+  {
+    title: "debug one",
+    body: "This is a test note to check if the array works!"
+  }
+];
+let noteslist = document.getElementById("noteslist");
+
+document.getElementById("save").addEventListener("click", save);
+function save() {
+  let titletext = prompt("Please enter a title for this note!:", "");
+  let bodytext = document.getElementById("notearea").value;
+  if (titletext == null || titletext == "") {
+    alert("No text detected; cancelling save");
+  } else {
+    alert("Successfully saved note as " + titletext + "!");
+    let newobj = new Object();
+    newobj["title"] = titletext;
+    newobj["body"] = bodytext;
+    notesArray.push(newobj);
+
+    let noteslist = document.getElementById("noteslist");
+    let object = notesArray[notesArray.length - 1];
+    let li = document.createElement("li");
+    let libut = document.createElement("button");
+    li.innerText = "";
+    libut.innerText = object.title;
+    li.appendChild(libut);
+    noteslist.appendChild(li);
+    li.setAttribute("id", "hi");
+    libut.addEventListener("click", retrieve);
+  }
+}
+
+let retrieve = function retrieve() {
+  let note = notesArray.find(
+    (noteobj) => noteobj.title === document.querySelector("#hi").innerText
+  );
+  document.getElementById("notearea").value = note["body"];
+};
